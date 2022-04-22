ce14a36fde63c12685e7258150a60e40c92e2e4c
Pedro A. Ventura <pedro-alexandre-silva@telecom.pt>
2022-04-22 20:00:00 +0100

    Async File Read and Write

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const { Console } = require('console');
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./start.txt', 'utf8');
+// const textIn = fs.readFileSync('./start.txt', 'utf8');
+// console.log(textIn);
+// const textOut = `First: ${textIn}`;
+// fs.writeFileSync('./output/end.txt', textOut);
+// fs.mkdirSync('./outputTest');
+// console.log(textOut);
 
-console.log(textIn);
+fs.readFile('./start.txt', 'utf8',  (err, data1) =>  {
+    if(err) {console.log(err);}
+    fs.readFile('./end.txt', 'utf8', (err, data2) => {
+        if(err) {console.log(err);}
+        fs.writeFile('./output2/concat.txt', data1 + data2, (err) => {
+            if (err) {console.log(err);}
+        });
+    });
+});
 
-const textOut = `First ${textIn}`;
-
-fs.writeFileSync('./end.txt', textOut);
+console.log('Will read file!');
